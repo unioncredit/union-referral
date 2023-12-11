4c44d2a88d0b8d9dcde9cd6d863d635c708f3153
twygod <twygod@gmail.com>
2023-12-11 18:58:04 +0800

    Feature/uni 1682 add accesscontrol unit tests (#1)
    * chg: add mock and testwrapper contract and
    
    * chg: add new error and modify visible types
    
    * new: add accessControl test
    
    * chg: add codecov and reports
    
    ---------
    
    Co-authored-by: Max Weng <max.m.weng@gmail.com>

---
+++ test/AccessControl.t.sol
@@ -0,0 +1,89 @@
+pragma solidity 0.8.19;
+
+import {TestWrapper} from "./TestWrapper.sol";
+import {AccessControl} from "src/AccessControl.sol";
+
+contract TestAccessControl is TestWrapper {
+    AccessControl public accessControl;
+    address public admin;
+    function setUp() public virtual {
+        admin = address(1);
+        deployMocks();
+        accessControl = new AccessControl(admin);
+    }
+
+    function testCannotSetPendingAdminNonAdmin() public {
+        vm.expectRevert(AccessControl.NotAdmin.selector);
+        accessControl.setPendingAdmin(address(2));
+    }
+
+    function testCannotSetPendingAdminNullAddress() public {
+        vm.startPrank(admin);
+        vm.expectRevert(AccessControl.NullAddress.selector);
+        accessControl.setPendingAdmin(address(0));
+        vm.stopPrank();
+    }
+
+    function testSetPendingAdmin(address newAdmin) public {
+        vm.assume(newAdmin!= address(0));
+        vm.startPrank(admin);
+        accessControl.setPendingAdmin(newAdmin);
+        assertEq(accessControl.pendingAdmin(), newAdmin);
+        vm.stopPrank();
+    }
+
+    function testCannotAcceptAdminNotPendingAdmin() public {
+        vm.prank(admin);
+        accessControl.setPendingAdmin(address(2));
+        vm.prank(address(3));
+        vm.expectRevert(AccessControl.NotPendingAdmin.selector);
+        accessControl.acceptAdmin();
+    }
+    
+    function testAcceptAdmin() public {
+        vm.prank(admin);
+        accessControl.setPendingAdmin(address(2));
+        vm.prank(address(2));
+        accessControl.acceptAdmin();
+        assertEq(accessControl.admin(), address(2));
+    }
+
+    function testCannotPauseNotAdmin() public {
+        vm.expectRevert(AccessControl.NotAdmin.selector);
+        accessControl.pause();
+    }
+
+    function testCannotPauseWhenPaused() public {
+        vm.startPrank(admin);
+        accessControl.pause();
+        vm.expectRevert(abi.encodeWithSelector(AccessControl.HasPaused.selector, true));
+        accessControl.pause();
+        vm.stopPrank();
+    }
+
+    function testPause() public {
+        vm.prank(admin);
+        accessControl.pause();
+        assertTrue(accessControl.paused());
+    }
+
+    function testCannotUnpauseNotAdmin() public {
+        vm.expectRevert(AccessControl.NotAdmin.selector);
+        accessControl.unpause();
+    }
+
+    function testCannotUnpauseWhenNotPaused() public {
+        vm.prank(admin);
+        vm.expectRevert(abi.encodeWithSelector(AccessControl.HasPaused.selector, false));
+        accessControl.unpause();
+    }
+
+    function testUnpause() public {
+        vm.startPrank(admin);
+        accessControl.pause();
+        assertTrue(accessControl.paused());
+        accessControl.unpause();
+        assertFalse(accessControl.paused());
+        vm.stopPrank();
+    }
+}
