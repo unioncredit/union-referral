5eedb08ba3035fe52d0cbacc66fdb6158bb888a7
twygod <twygod@gmail.com>
2024-01-26 14:45:14 +0800

    chg: Modify the RegisterHelper contract to support transferring eth to the contract

--- test/TestWrapper.sol
+++ test/TestWrapper.sol
@@ -3,13 +3,16 @@ pragma solidity 0.8.19;
 import "forge-std/Test.sol";
 import {UnionTokenMock} from "src/mocks/UnionTokenMock.sol";
 import {UserManagerMock} from "src/mocks/UserManagerMock.sol";
+import {GnosisSafeProxyMock} from "src/mocks/GnosisSafeProxyMock.sol";
 
 contract TestWrapper is Test {
     UnionTokenMock public unionTokenMock;
     UserManagerMock public userManagerMock;
+    GnosisSafeProxyMock public gnosisSafeProxyMock;
 
     function deployMocks() public {
         unionTokenMock = new UnionTokenMock();
         userManagerMock = new UserManagerMock(address(unionTokenMock));
+        gnosisSafeProxyMock = new GnosisSafeProxyMock(address(11));
     }
 }
